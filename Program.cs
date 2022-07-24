c3408f9b0a6c69349962bd362a08b7026a99ca38
Marina Chernykh <danmarizza@gmail.com>
2022-07-24 13:02:42 +0300

    code structure added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] CreateNewArray(string[] inputArray)
+{
+
+    return outputArray;
+}
+
+string[] FillAray(string[] inputArray, string[] outputArray)
+{
+    return outputArray;
+}
+
+void PrintArray(string[] array)
+{
+
+}
+
+
+string[] array = {"hello", "2", "world", ":-)"};
+string[] createdArray = CreateNewArray(array);
+FillAray(array, createdArray);
+PrintArray(array);
+PrintArray(createdArray);
